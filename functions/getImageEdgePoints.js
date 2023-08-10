504bf6942eace266653c981806b68fd2b03da447
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2023-08-10 16:35:36 +0200

    Changed edge points values to procentage and object instead of array

--- functions/getImageEdgePoints.js
+++ functions/getImageEdgePoints.js
@@ -1,92 +1,108 @@
-const getImageOpacityMatrix = require('./getImageOpacityMatrix');
+const getImageOpacityMatrix = require("./getImageOpacityMatrix");
 
 const getImageTransparencyMatrix = (imageOpacityMatrix) => {
- return imageOpacityMatrix.map((row) => {
-  return row.map((opacity) => !opacity);
- });
+  return imageOpacityMatrix.map((row) => {
+    return row.map((opacity) => !opacity);
+  });
 };
 
 const getRightEdgePoints = (matrix) => {
- const height = matrix.length;
- const width = matrix[0].length;
- const edgePoints = [];
- let point = null;
- for (let y = 0; y < height; y++) {
-  for (let x = 0; x < width - 1; x++) {
-   if (matrix[y][x] && !matrix[y][x + 1]) point = [x, y];
+  const height = matrix.length;
+  const width = matrix[0].length;
+  const edgePoints = [];
+  let point = null;
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width - 1; x++) {
+      if (matrix[y][x] && !matrix[y][x + 1]) point = [x, y];
+    }
+    if (point) edgePoints.push(point);
+    point = null;
   }
-  if (point) edgePoints.push(point);
-  point = null;
- }
- return edgePoints;
+  return edgePoints;
 };
 
 const getLeftEdgePoints = (matrix) => {
- const height = matrix.length;
- const width = matrix[0].length;
- const edgePoints = [];
- let point = null;
- for (let y = 0; y < height; y++) {
-  for (let x = width; x > 0; x--) {
-   if (matrix[y][x] && !matrix[y][x - 1]) point = [x, y];
+  const height = matrix.length;
+  const width = matrix[0].length;
+  const edgePoints = [];
+  let point = null;
+  for (let y = 0; y < height; y++) {
+    for (let x = width; x > 0; x--) {
+      if (matrix[y][x] && !matrix[y][x - 1]) point = [x, y];
+    }
+    if (point) edgePoints.push(point);
+    point = null;
   }
-  if (point) edgePoints.push(point);
-  point = null;
- }
- return edgePoints;
+  return edgePoints;
 };
 
 const getBottomEdgePoints = (matrix) => {
- const height = matrix.length;
- const width = matrix[0].length;
- const edgePoints = [];
- let point = null;
- for (let x = 0; x < width; x++) {
-  for (let y = 0; y < height - 1; y++) {
-   if (matrix[y][x] && !matrix[y + 1][x]) point = [x, y];
+  const height = matrix.length;
+  const width = matrix[0].length;
+  const edgePoints = [];
+  let point = null;
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height - 1; y++) {
+      if (matrix[y][x] && !matrix[y + 1][x]) point = [x, y];
+    }
+    if (point) edgePoints.push(point);
+    point = null;
   }
-  if (point) edgePoints.push(point);
-  point = null;
- }
- return edgePoints;
+  return edgePoints;
 };
 
 const getTopEdgePoints = (matrix) => {
- const height = matrix.length;
- const width = matrix[0].length;
- const edgePoints = [];
- let point = null;
- for (let x = 0; x < width; x++) {
-  for (let y = height - 1; y > 0; y--) {
-   if (matrix[y][x] && !matrix[y - 1][x]) point = [x, y];
+  const height = matrix.length;
+  const width = matrix[0].length;
+  const edgePoints = [];
+  let point = null;
+  for (let x = 0; x < width; x++) {
+    for (let y = height - 1; y > 0; y--) {
+      if (matrix[y][x] && !matrix[y - 1][x]) point = [x, y];
+    }
+    if (point) edgePoints.push(point);
+    point = null;
   }
-  if (point) edgePoints.push(point);
-  point = null;
- }
- return edgePoints;
+  return edgePoints;
 };
 
 const isPointInArray = (array, point) => {
- return array.some((p) => p[0] === point[0] && p[1] === point[1]);
+  return array.some((p) => p[0] === point[0] && p[1] === point[1]);
 };
 
 const getMergedPointsWithoutDuplicates = (points) => {
- const merged = [];
- points.forEach((point) => {
-  if (!isPointInArray(merged, point)) merged.push(point);
- });
- return merged;
+  const merged = [];
+  points.forEach((point) => {
+    if (!isPointInArray(merged, point)) merged.push(point);
+  });
+  return merged;
 };
 
 const getImageEdgePoints = async (directory, imageName, extension) => {
- const imageOpacityMatrix = await getImageOpacityMatrix(directory, imageName, extension);
- const imageTransparencyMatrix = getImageTransparencyMatrix(imageOpacityMatrix);
- const rigthEdgePoints = getRightEdgePoints(imageTransparencyMatrix);
- const leftEdgePoints = getLeftEdgePoints(imageTransparencyMatrix);
- const bottomEdgePoints = getBottomEdgePoints(imageTransparencyMatrix);
- const topEdgePoints = getTopEdgePoints(imageTransparencyMatrix);
- const mergedPointsWithoutDuplicates = getMergedPointsWithoutDuplicates([...rigthEdgePoints, ...leftEdgePoints, ...topEdgePoints, ...bottomEdgePoints]);
- return mergedPointsWithoutDuplicates;
+  const imageOpacityMatrix = await getImageOpacityMatrix(
+    directory,
+    imageName,
+    extension
+  );
+  const imageTransparencyMatrix =
+    getImageTransparencyMatrix(imageOpacityMatrix);
+
+  const rigthEdgePoints = getRightEdgePoints(imageTransparencyMatrix);
+  const leftEdgePoints = getLeftEdgePoints(imageTransparencyMatrix);
+  const bottomEdgePoints = getBottomEdgePoints(imageTransparencyMatrix);
+  const topEdgePoints = getTopEdgePoints(imageTransparencyMatrix);
+
+  const mergedPointsWithoutDuplicates = getMergedPointsWithoutDuplicates([
+    ...rigthEdgePoints,
+    ...leftEdgePoints,
+    ...topEdgePoints,
+    ...bottomEdgePoints,
+  ]);
+  return {
+    edgePoints: mergedPointsWithoutDuplicates,
+    imageWidth: imageTransparencyMatrix[0].length,
+    imageHeight: imageTransparencyMatrix.length,
+  };
 };
 
 module.exports = getImageEdgePoints;
--- index.js
+++ index.js
@@ -1,118 +1,148 @@
 var fs = require('fs');
 PNG = require('pngjs').PNG;
 const getImageEdgePoints = require('./functions/getImageEdgePoints');
+const directory = './images/';
+let files = fs.readdirSync(directory).map((file) => {
+  const [imageName, extension] = file.split('.');
+  return { imageName, extension };
+});
 
-const findBorderValues = (edgePoints) => {
- let minX = edgePoints[0][0];
- let maxX = edgePoints[0][0];
- let minY = edgePoints[0][1];
- let maxY = edgePoints[0][1];
- edgePoints.forEach((point) => {
-  if (point[0] < minX) minX = point[0];
-  if (point[0] > maxX) maxX = point[0];
-  if (point[1] < minY) minY = point[1];
-  if (point[1] > maxY) maxY = point[1];
- });
- return { minX, maxX, minY, maxY };
+// const findBorderValues = (edgePoints) => {
+//  let minX = edgePoints[0][0];
+//  let maxX = edgePoints[0][0];
+//  let minY = edgePoints[0][1];
+//  let maxY = edgePoints[0][1];
+//  edgePoints.forEach((point) => {
+//   if (point[0] < minX) minX = point[0];
+//   if (point[0] > maxX) maxX = point[0];
+//   if (point[1] < minY) minY = point[1];
+//   if (point[1] > maxY) maxY = point[1];
+//  });
+//  return { minX, maxX, minY, maxY };
+// };
+
+// const getPointsMap = (points) => {
+//  const map = new Map();
+//  points.forEach((point) => {
+//   if (map.has(point[0]))
+//    map.set(
+//     point[0],
+//     [...map.get(point[0]), point[1]].sort((y1, y2) => y1 - y2)
+//    );
+//   else map.set(point[0], [point[1]]);
+//  });
+//  return new Map([...map].sort(([key1, value1], [key2, value2]) => key1 - key2));
+// };
+
+//***** PROCESS EDGE POINTS */
+
+const changePointsToObjects = (edgePoints) => {
+  return edgePoints.map((point) => ({ x: point[0], y: point[1] }));
 };
 
-const makeEdgeImage = (img, points) => {
- points.forEach((point) => {
-  const idx = (img.width * point[1] + point[0]) << 2;
-  setOpacity(img, idx, 255);
- });
+const getAngleBetweenTwoPoints = (point1, point2) => {
+  let angle = Math.atan2(point2.y - point1.y, point2.x - point1.x);
+  if (angle < 0) angle += 2 * Math.PI;
+  return angle;
 };
 
-const setOpacity = (img, idx, opacity) => {
- img.data[idx + 3] = opacity;
+const sortPointsCounterClockwise = (edgePoints, width, height) => {
+  const imageCenter = { x: width / 2, y: height / 2 };
+  return edgePoints.sort((point1, point2) => getAngleBetweenTwoPoints(imageCenter, point2) - getAngleBetweenTwoPoints(imageCenter, point1));
 };
 
-const createEdgePreview = (directory, imageName, extension, edgePoints) => {
- fs
-  .createReadStream(directory + imageName + '.' + extension)
-  .pipe(
-   new PNG({
-    filterType: 4,
-   })
-  )
-  .on('parsed', function () {
-   for (var y = 0; y < this.height; y++) {
-    for (var x = 0; x < this.width; x++) {
-     const currIdx = (this.width * y + x) << 2;
-     setOpacity(this, currIdx, 0);
-    }
-   }
-   makeEdgeImage(this, edgePoints);
-   this.pack().pipe(fs.createWriteStream(`./edge_previews/${imageName}EdgePreviev.${extension}`));
+const reduceAmountOfPoints = (edgePoints) => {
+  return edgePoints.reduce((acc, point, index) => {
+    if (index % 3 === 0) acc.push(point);
+    return acc;
+  }, []);
+};
+
+const changePointsToProcentage = (edgePoints, width, height) => {
+  return edgePoints.map((point) => ({ x: Math.round((point.x / width) * 100), y: Math.round((point.y / height) * 100) }));
+};
+
+//***** MAKE EDGE IMAGE PREVIEW */
+
+const makeEdgeImage = (img, edgePoints) => {
+  edgePoints.forEach((point) => {
+    const idx = (img.width * point.y + point.x) << 2;
+    setOpacity(img, idx, 255);
   });
 };
 
-const getPointsMap = (points) => {
- const map = new Map();
- points.forEach((point) => {
-  if (map.has(point[0]))
-   map.set(
-    point[0],
-    [...map.get(point[0]), point[1]].sort((y1, y2) => y1 - y2)
-   );
-  else map.set(point[0], [point[1]]);
- });
- return new Map([...map].sort(([key1, value1], [key2, value2]) => key1 - key2));
+const setOpacity = (img, idx, opacity) => {
+  img.data[idx + 3] = opacity;
 };
 
-const generateText = (map, borderValues, imageName) => {
- let text = `${imageName}: {\n
- minX: ${borderValues.minX},
- maxX: ${borderValues.maxX},
- minY: ${borderValues.minY},
- maxY: ${borderValues.maxY},
-	collisionPoints: new Map(\n`;
- [...map].forEach(([key, value]) => {
-  text += `[${key}, ${JSON.stringify(value)}],\n`;
- });
- text += `)\n},\n`;
- return text;
+const createEdgePreview = (edgePoints, directory, imageName, extension) => {
+  fs.createReadStream(directory + imageName + '.' + extension)
+    .pipe(
+      new PNG({
+        filterType: 4,
+      }),
+    )
+    .on('parsed', function () {
+      for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+          const currIdx = (this.width * y + x) << 2;
+          setOpacity(this, currIdx, 0);
+        }
+      }
+      makeEdgeImage(this, edgePoints);
+      this.pack().pipe(fs.createWriteStream(`./edge_previews/${imageName}EdgePreviev.${extension}`));
+    });
 };
+
+//***** GENERATE TEXT */
+
+const generateText = (edgePoints, imageName) => {
+  let text = `'${imageName}': [\n`;
+  edgePoints.forEach((point) => {
+    text += `{x:${point.x}, y:${point.y}},\n`;
+  });
+  text += `\n],\n`;
+  return text;
+};
+
+//***** CREATE JS FILE */
 
 const createJavascriptFile = (fileName, text) => {
- fs.writeFile(`./output_js/${fileName}.js`, text, (err) => {
-  if (err) console.log('ups');
- });
+  fs.writeFile(`./output_js/${fileName}.js`, text, (err) => {
+    if (err) console.log('ups');
+  });
 };
 
-const directory = './images/';
-
-let files = fs.readdirSync(directory).map((file) => {
- const [imageName, extension] = file.split('.');
- return { imageName, extension };
-});
-
 const getEdgePoints = async () => {
- const promises = files.map(async (file) => {
-  const edgePoints = await getImageEdgePoints(directory, file.imageName, file.extension);
-  const borderValues = findBorderValues(edgePoints);
-  const pointsMap = getPointsMap(edgePoints);
-  const text = generateText(pointsMap, borderValues, file.imageName);
-  file.borderValues = borderValues;
-  file.edgePoints = edgePoints;
-  file.text = text;
-  createEdgePreview(directory, file.imageName, file.extension, edgePoints);
-  return file;
- });
+  const promises = files.map(async (file) => {
+    const { edgePoints, imageWidth, imageHeight } = await getImageEdgePoints(directory, file.imageName, file.extension);
+    const edgePointsAsObject = changePointsToObjects(edgePoints);
+    const edgePointsSorted = sortPointsCounterClockwise(edgePointsAsObject, imageWidth, imageHeight);
+    const edgePointsReduced = reduceAmountOfPoints(edgePointsSorted);
+    createEdgePreview(edgePointsReduced, directory, file.imageName, file.extension);
+    const edgePointsInProcentage = changePointsToProcentage(edgePointsReduced, imageWidth, imageHeight);
+    const text = generateText(edgePointsInProcentage, file.imageName);
+    file.text = text;
+    return file;
+    //   const borderValues = findBorderValues(edgePoints);
+    //   const pointsMap = getPointsMap(edgePoints);
+    //   file.borderValues = borderValues;
+    //   file.edgePoints = edgePoints;
+  });
 
- files = await Promise.all(promises);
+  files = await Promise.all(promises);
 
- let text = files.reduce((acc, curr) => {
-  return acc + curr.text;
- }, '');
+  let text = files.reduce((acc, curr) => {
+    return acc + curr.text;
+  }, '');
 
- text = `// in collisionPoints Map key is x coordinate and value is an array of y coordinates\n\n
- const imagesCollisionData = {\n
-	${text}}\n
-	export default imagesCollisionData
- `;
+  text = `// x and y are collision points coordinates in % of width and height of image\n\n
+   const imagesCollisionData = {\n
+  	${text}}\n
+  	export default imagesCollisionData
+   `;
 
- createJavascriptFile('imagesCollisionData', text);
+  createJavascriptFile('images_collision_data', text);
 };
 
 getEdgePoints();
